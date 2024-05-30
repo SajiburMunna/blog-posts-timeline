d2a145386034554fbd1f56b78909d8ad5e8168ae
sajiburmunna <sajiburmunna087@gmail.com>
2024-05-31 01:10:58 +0600

    feat(footer): design and added sociallink

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,61 @@
+import { PropsWithChildren } from "react";
+
+import Facebook from "../assets/facebook";
+import Github from "../assets/github";
+import Logo from "../assets/logo";
+import Twitter from "../assets/twitter";
+
+const SocialLinkData = [
+  {
+    id: 1,
+    href: "http://github.com/sajiburmunna",
+    icon: Github,
+  },
+  {
+    id: 2,
+    href: "http://github.com/sajiburmunna",
+    icon: Facebook,
+  },
+  {
+    id: 3,
+    href: "http://github.com/sajiburmunna",
+    icon: Twitter,
+  },
+];
+
+function Footer() {
+  return (
+    <footer className="py-[26px] bg-[#111B21] border-t-[1.5px] border-[#FFFFFF1A]">
+      <div className="max-w-[1440px] m-auto px-[50px] md:px-[120px]">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <Logo />
+            <p className="text-base leading-5">JSojibcript.BD</p>
+          </div>
+          <div>
+            <div className="flex gap-2">
+              {SocialLinkData.map(({ href, icon: Icon, id }) => (
+                <SocialLink key={id} href={href}>
+                  <Icon />
+                </SocialLink>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
+const SocialLink = ({
+  href,
+  children,
+}: PropsWithChildren<{ href: string }>) => {
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  );
+};
--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,14 +1,18 @@
 import { PropsWithChildren } from "react";
 
 import Logo from "../assets/logo";
+import Footer from "./Footer";
 
 function PageLayout({ children }: PropsWithChildren<unknown>) {
   return (
-    <div className="px-[120px]">
-      <header className="py-[26px]">
-        <Logo />
-      </header>
-      <main>{children}</main>
+    <div>
+      <div className="px-[50px] md:px-[120px] mb-[96px]">
+        <header className="py-[26px]">
+          <Logo />
+        </header>
+        <main>{children}</main>
+      </div>
+      <Footer />
     </div>
   );
 }
